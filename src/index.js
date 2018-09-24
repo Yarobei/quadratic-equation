f2c42d99d4fec988707e7f817ea45128511ef061
Dzmitry Bahadziazh <dima3911784@hotmail.com>
2018-09-24 21:42:58 +0300

    solve quadratic equation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
-}
+    const arr = equation.split(' ');
+    const solutions = [];
+    
+    let a = +arr[0];
+    let b = 0;
+    let c = 0;
+    if (arr[3] === '-') {
+      b = -arr[4];
+    } else {b = +arr[4];}
+    if (arr[7] === '-') {
+      c = -arr[8];
+    } else {c = +arr[8];}
+    
+    let x1 = 0;
+    let x2 = 0;
+    const discr = b * b - (4 * a * c);
+    
+    if (discr > 0) {
+      x1 = Math.round((-b + Math.sqrt(discr)) / (2 * a));
+      x2 = Math.round((-b - Math.sqrt(discr)) / (2 * a));
+    } else if (discr === 0) {
+      x1 = Math.round(-b / (2 * a));
+      x2 = x1;
+    }
+    
+    if (x1 > x2) {
+      solutions.push(x2);
+      solutions.push(x1);
+    } else {
+      solutions.push(x1);
+      solutions.push(x2);
+    }
+    return solutions;
+  }
